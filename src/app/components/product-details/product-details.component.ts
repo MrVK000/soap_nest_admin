590421abdd3ded4aaaba657084dc0f53ae95154d
Vaikundamuthu <vaikundamuthu1430@gmail.com>
2026-03-15 00:43:09 +0530

    Refactored product details page

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -5,23 +5,26 @@ import { Subject, takeUntil } from 'rxjs';
 import { Review } from '../../interfaces/interfaces';
 import { CommonModule } from '@angular/common';
 import { SharedService } from '../../services/shared.service';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CommonModule],
+  imports: [CommonModule, CarouselModule, TagModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
 export class ProductDetailsComponent {
   private destroy$ = new Subject<void>();
   productId: string;
-  productKeys: string[] = [];
-  productValues: string[] = [];
+  product: any = null;
   reviews: Review[] = [];
+  baseUrl = environment.apiBaseUrl.replace('/api/v1/', '');
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private api: ApiService, public sharedService: SharedService) {
-    if (!(this.activatedRoute?.snapshot?.paramMap?.get('id'))) this.router.navigate(['/products']);
-    this.productId = (this.activatedRoute?.snapshot?.paramMap?.get('id')) as string;
+    if (!this.activatedRoute?.snapshot?.paramMap?.get('id')) this.router.navigate(['/products']);
+    this.productId = this.activatedRoute?.snapshot?.paramMap?.get('id') as string;
   }
 
   ngOnInit(): void {
@@ -30,21 +33,17 @@ export class ProductDetailsComponent {
 
   getProduct() {
     this.api.getProduct(this.productId).pipe(takeUntil(this.destroy$)).subscribe((res: any) => {
-      const { reviews: reviews, ...productWithOutResult } = res?.data;
-      this.reviews = reviews;
-      this.extractProductDetails(productWithOutResult);
-    })
+      this.product = res?.data;
+      this.reviews = res?.data?.reviews ?? [];
+    });
   }
 
-  extractProductDetails(productWithOutResult: any) {
-    this.productKeys = Object.keys(productWithOutResult);
-    this.productValues = Object.values(productWithOutResult);
+  getImageUrl(path: string): string {
+    return this.baseUrl + path;
   }
 
-  isValidDate(valueString: string): boolean {
-    if (Number(valueString)) return false;
-    const parsedDate = Date.parse(valueString);
-    return !isNaN(parsedDate);
+  openImage(path: string): void {
+    window.open(this.getImageUrl(path), '_blank');
   }
 
   ngOnDestroy(): void {
